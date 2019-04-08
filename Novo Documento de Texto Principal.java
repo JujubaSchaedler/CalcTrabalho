666e9c0e64f032efb5e487abc89115ee7c1d9c11
JujubaSchaedler <juliaschaedler@gmail.com>
2019-04-08 14:40:07 -0300

    Add a segunda parte

--- Novo Documento de Texto Principal.java
+++ Novo Documento de Texto Principal.java
@@ -29,3 +29,46 @@ public class PrincipalCalculadora extends JFrame {
 	Calculadora ca = new Calculadora();
 	double num1;
 	double num2;
+private final ButtonGroup buttonGroup = new ButtonGroup();
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					PrincipalCalculadora frame = new PrincipalCalculadora();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public PrincipalCalculadora() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 309, 506);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Calculadora");
+		lblNewLabel.setFont(new Font("Yu Gothic UI Semibold", Font.PLAIN, 16));
+		lblNewLabel.setBounds(91, 11, 96, 22);
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblNmero = new JLabel("N\u00FAmero 1:");
+		lblNmero.setFont(new Font("Yu Gothic UI Semibold", Font.PLAIN, 16));
+		lblNmero.setBounds(22, 79, 96, 22);
+		contentPane.add(lblNmero);
+		
+		JLabel lblNmero_1 = new JLabel("N\u00FAmero 2:");
+		lblNmero_1.setFont(new Font("Yu Gothic UI Semibold", Font.PLAIN, 16));
+		lblNmero_1.setBounds(22, 132, 96, 22);
+		contentPane.add(lblNmero_1);
+
